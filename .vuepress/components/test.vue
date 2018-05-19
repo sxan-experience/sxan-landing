ef686db4788540f14b7cf1d9916068cb4f816c21
births <irth03@gmail.com>
2018-05-19 14:10:36 -0400

    no unit tests

--- .vuepress/components/test.vue
+++ .vuepress/components/test.vue
@@ -1,3 +1,15 @@
 <template>
     <h1>Hello</h1>
 </template>
+
+<!-- On another page
+
+<div>
+<test/>
+<button @click="show = !show">
+    Show me the data
+</button>
+</div>
+<div v-if="show">{{joke ? joke : 'waiting...'}}</div> 
+
+-->
